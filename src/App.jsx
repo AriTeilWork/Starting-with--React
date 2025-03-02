e86deb5fba9ed77237b0fb8dc7a0e33d6f0740d2
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-02 16:38:30 +0200

    copy the files to src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-const App = () => (
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
 
-  <div>
-
-    <p>Hello world</p>
-
-  </div>
-
-)
-
-export default App
+export default App;
