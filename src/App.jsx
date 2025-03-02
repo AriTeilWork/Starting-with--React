647043144b26bb312a901b6d3a9a5f3dedea35a8
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-02 18:18:09 +0200

    Alternative Props Syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import {CORE_CONCEPTS} from './data';
 
 const ReactDecsriptions = ['Fundamental','Crucial','Core'];
 
@@ -27,7 +28,7 @@ function CoreConcepts(props) {
   return( <li>
     <img src={props.image} alt={props.title}/>
     <h3>{props.title}</h3>
-    <P>{props.description}</P>
+    <p>{props.description}</p>
   </li>
   );
 }
@@ -37,17 +38,27 @@ function App() {
     <div>
       <Header></Header>
       <main>
-        <section id='core-concepts'></section>
+        <section id='core-concepts'>
         <h2>Core Consepts</h2>
-        <ul>
-          <CoreConcepts
-            title="Components"
-            description="The core UI building block."
-            image={componentsImg}/>
-          <CoreConcepts title="props"/>
-          <CoreConcepts/>
-          <CoreConcepts/>
-        </ul>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
