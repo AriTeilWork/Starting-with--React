a6102ec98693e630ee7fbf3f58606ad760fe5fce
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-04 17:00:10 +0200

    Using State TodoList

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ul>
+  );
+}
